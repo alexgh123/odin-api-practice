a8c139a5ac1cacc37fb3b4d25dc0ed803c18745d
Alex Hardt <hardt.alex@gmail.com>
2015-05-28 10:42:30 -0500

    understanding the flickraw methods better, notes in flickr helper to pull the right data

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -1,5 +1,16 @@
 class KittensController < ApplicationController
+
+  include FlickrHelper
+
   def index
     @kittens = Kitten.all
+
+    @photo_ish_things = user_photos('132826669@N04')
+
+    # url = params[:url]
+    #   photoset_id = find_photoset_id_by_user_url( url )
+    #   @photo_ids = photos_in_set(photoset_id)
+    #   @photo = photo_url(@photo_ids.sample)
+
   end
 end
--- app/helpers/flickr_helper.rb
+++ app/helpers/flickr_helper.rb
@@ -1,20 +1,44 @@
 module FlickrHelper
 
-  def user_photos(user_id, photo_count=12 )
-    flickr.photos.search(:user_id => user_id, :per_page => photo_count)
-  end
-  # welp, flickr.yml is wrong because im not using it
+def user_photos(user_id)
+  flickr.photos.search(:user_id => user_id)
+end #returns the user's photo-ish things
 
-  # im not sure these helper methods will be correct b/c i deviated from the pixellated visions blog when i used flickraw in stead of flick_ru
+def phototset
+  flickr.photosets.getList(user_id: user.id).to_a.sample.id
+end
 
-  def render_flickr_sidebar_widget(user_id, photo_count=12, columns =2)
-    begin
-      photos = user_photos(user_id, photo_count).in_groups_of(2)
-      render :partial => '/flickr/sidebar_widget', :locals => {:photos => photos}
-    rescue Exception
-      render :partial => '/flickr/unavailable'
-    end
-  end
+# to get the actual photo an the information that is actually useful....
+
+
+#returns the photo id of the 3rd photo of the photo like things
+flickr.photos.search(:user_id=>'132826669@N04').to_a[3].id
+
+#returns all kind of info about the actual photo
+flickr.photos.getInfo(:photo_id =>"17974079119")
+
+#returns the actual photo
+
+flickr.photos.getInfo(:photo_id =>"17974079119").urls[0]._content
+
+
+
+  # def find_photoset_id_by_user_url(url)
+
+  #   # flickr.photos.search(user_id: user_id, per_page: photo_count)
+
+  #   user = flickr.urls.lookupUser( url: url )
+  #   random_photoset = flickr.photosets.getList(user_id: user.id).to_a.sample.id # an array of objects of photo albums, each albums may contain many photos
+  # end
+
+  # def photos_in_set(photo_set_id)
+  #   flickr.photosets.getPhotos( photoset_id: photo_set_id ).photo.map { |image| image.id  } # an array of photo id
+  # end
+
+  # def photo_url(photo_id)
+  #   photo = flickr.photos.getInfo(photo_id: photo_id)
+  #   FlickRaw.url_b(photo)
+  # end
 
 
 end
