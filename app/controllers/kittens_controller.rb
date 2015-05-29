38f4ae58c14af1ae802cc74e81c06646533299fa
Alex Hardt <hardt.alex@gmail.com>
2015-05-29 08:44:44 -0500

    removed comments

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -7,12 +7,6 @@ class KittensController < ApplicationController
 
     @photo_url_array = array_of_photo_urls('132826669@N04')
 
-    # @photo_ish_things = user_photos('132826669@N04')
-
-    # url = params[:url]
-    #   photoset_id = find_photoset_id_by_user_url( url )
-    #   @photo_ids = photos_in_set(photoset_id)
-    #   @photo = photo_url(@photo_ids.sample)
 
   end
 end
--- app/helpers/flickr_helper.rb
+++ app/helpers/flickr_helper.rb
@@ -1,9 +1,5 @@
 module FlickrHelper
 
-  #this is so bad and needs to be cleaned up
-
-  # I should really be able to have two variables, user id and photo id
-
   def array_of_photo_urls(user_id)
     id_array = []
     photo_array = flickr.photos.search(:user_id=>user_id).to_a
@@ -17,71 +13,9 @@ module FlickrHelper
     url_array
   end
 
-    #need this one
   def photo_url(photo_id)
     flickr.photos.getInfo(:photo_id =>photo_id).urls[0]._content
   end
-
-  #### less important below
-
-  # def array_of_all_photo_ids_of_user(user_id)
-  #   id_array = []
-  #   photo_array = flickr.photos.search(:user_id=>user_id).to_a
-  #   photo_array.each do |x|
-  #     id_array << x.id
-  #   end
-  #   id_array
-  # end
-
-
-
-  ###### not as important, but instructive for other uses of api ####
-
-  # def array_of_photo_info(user_id)
-  #   flickr.photos.search(:user_id=>user_id).to_a
-  # end
-
-  # def photo_info(photo_id)
-  #   flickr.photos.getInfo(:photo_id =>photo_id)
-  # end
-
-
-
-
-
-# def user_photos(user_id)
-#   flickr.photos.search(:user_id => user_id)
-# end #returns the user's photo-ish things
-
-# def phototset
-#   flickr.photosets.getList(user_id: user.id).to_a.sample.id
-# end
-
-#returns all kind of info about the actual photo
-# flickr.photos.getInfo(:photo_id =>"17974079119")
-
-#returns the actual photo
-
-# flickr.photos.getInfo(:photo_id =>"17974079119").urls[0]._content
-
-
-
-  # def find_photoset_id_by_user_url(url)
-
-  #   # flickr.photos.search(user_id: user_id, per_page: photo_count)
-
-  #   user = flickr.urls.lookupUser( url: url )
-  #   random_photoset = flickr.photosets.getList(user_id: user.id).to_a.sample.id # an array of objects of photo albums, each albums may contain many photos
-  # end
-
-  # def photos_in_set(photo_set_id)
-  #   flickr.photosets.getPhotos( photoset_id: photo_set_id ).photo.map { |image| image.id  } # an array of photo id
-  # end
-
-  # def photo_url(photo_id)
-  #   photo = flickr.photos.getInfo(photo_id: photo_id)
-  #   FlickRaw.url_b(photo)
-  # end
 
 
 end
