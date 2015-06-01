7c9dc942ddba778d4996615679169f9809912fd7
Alex Hardt <hardt.alex@gmail.com>
2015-06-01 10:17:39 -0500

    why dont https images load!?

--- config/environment.rb
+++ config/environment.rb
@@ -10,8 +10,6 @@ require 'flickraw'
 FlickRaw.api_key=ENV["flickr_api_key"]
 FlickRaw.shared_secret=ENV["flickr_api_secret"]
 
-
-
 list   = flickr.photos.getRecent
 
 id     = list[0].id
@@ -25,4 +23,6 @@ sizes = flickr.photos.getSizes :photo_id => id
 
 original = sizes.find {|s| s.label == 'Original' }
 puts original.width       # => "800" -- may fail if they have no original marked image
+
+# FlickRaw.secure = false #https (emphasis on 's') images aren't loading on my local dev env and i want to see the pictures
 
