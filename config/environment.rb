ddec7a70a54d79eef564ff428f82124b8122c926
Alex Hardt <hardt.alex@gmail.com>
2015-06-01 16:09:28 -0500

    removed the line with the width method bc heroku didnt like it

--- config/environment.rb
+++ config/environment.rb
@@ -22,7 +22,7 @@ puts info.dates.taken     # => "2006-07-06 15:16:18"
 sizes = flickr.photos.getSizes :photo_id => id
 
 original = sizes.find {|s| s.label == 'Original' }
-puts original.width       # => "800" -- may fail if they have no original marked image
+# puts original.width       # => "800" -- may fail if they have no original marked image
 
 # FlickRaw.secure = false #https (emphasis on 's') images aren't loading on my local dev env and i want to see the pictures
 
